fe4580e6826cbedcc18dd6a73e943c1a7ded543a
Seven <phoenixpyra7@gmail.com>
2024-01-01 15:33:36 -0600

    added first prompt section with no exact detail as a place holder.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+const prompt = inquirer.createPromptModule();
+
+
+prompt ([
+    {
+
+        type: "input",
+        name: "name",
+        message: "ENTER QUESTION HERE?",
+
+    }
+])
